d074f8a2c8d384839d19fe8b834a381ee3af5ba9
mstfmrt07 <60856457+mstfmrt07@users.noreply.github.com>
2023-04-21 22:35:54 +0300

    Input Controller bug fix

--- src/InputController.h
+++ src/InputController.h
@@ -10,31 +10,46 @@ public:
 	{
         //Keyboard events
         if (event.type == Event::KeyPressed)
-        {
-            if (event.key.code == Keyboard::Left || event.key.code == Keyboard::A)
-                horizontalInput = -1;
-            else if (event.key.code == Keyboard::Right || event.key.code == Keyboard::D)
-                horizontalInput = 1;
-
-            if (event.key.code == Keyboard::Down || event.key.code == Keyboard::S)
-                pressingDown = true;
-            else
-                pressingDown = false;
-
-            if (event.key.code == Keyboard::Up || event.key.code == Keyboard::W)
-                rotating = true;
-            else
-                rotating = false;
-        }
-        else //If there is no keyboard event
-        {
-            pressingDown = false;
-            rotating = false;
-            horizontalInput = 0;
-        }
+            HandleKeyPressed(event.key.code);
+        else if(event.type == Event::KeyReleased)
+            HandleKeyReleased(event.key.code);
 	}
 
     int horizontalInput = 0;
+    bool hardDrop = false;
     bool pressingDown = false;
     bool rotating = false;
+
+private:
+    void HandleKeyPressed(Keyboard::Key& key)
+    {
+        if (key == Keyboard::Left || key == Keyboard::A)
+            horizontalInput = -1;
+        else if (key == Keyboard::Right || key == Keyboard::D)
+            horizontalInput = 1;
+
+        if (key == Keyboard::Down || key == Keyboard::S)
+            pressingDown = true;
+
+        if (key == Keyboard::Up || key == Keyboard::W)
+            rotating = true;
+
+        if (key == Keyboard::X)
+            hardDrop = true;
+    }
+
+    void HandleKeyReleased(Keyboard::Key& key)
+    {
+        if (key == Keyboard::Left || key == Keyboard::A || key == Keyboard::Right || key == Keyboard::D)
+            horizontalInput = 0;
+
+        if (key == Keyboard::Down || key == Keyboard::S)
+            pressingDown = false;
+
+        if (key == Keyboard::Up || key == Keyboard::W)
+            rotating = false;
+
+        if (key == Keyboard::X)
+            hardDrop = false;
+    }
 };
